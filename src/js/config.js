d35e3c7df244c9556ad53b6be62b0100187953d0
frankowskim <m.frankowski@hotmail.com>
2024-03-24 13:08:38 +0100

    first strinke

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '40014c37-9645-4254-82af-801d74011b59';
+export const MODAL_CLOSE_SEC = 2.5;
